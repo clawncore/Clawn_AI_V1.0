c85b0a2d92f5f5a8927947d675217a0df510d75a
Simbisai Chinhema <simbisaichinhema14@gmail.com>
2026-05-16 23:09:32 +0530

    Add ESM shims for __dirname and __filename

--- tsup.config.ts
+++ tsup.config.ts
@@ -19,7 +19,11 @@ export default defineConfig({
   banner: {
     js: `
 import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 `,
   },
   noExternal: [/.*/],
